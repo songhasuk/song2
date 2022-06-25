16e1260201a56e1b1ef7fb77795992c4c2d4ca49
TaeJoon Jeon <46275680+DingDongDeng@users.noreply.github.com>
2022-06-25 23:09:05 +0900

    Update LonginServiceProc.java

--- src/main/java/com/movie/test/service/impl/LonginServiceProc.java
+++ src/main/java/com/movie/test/service/impl/LonginServiceProc.java
@@ -11,8 +11,8 @@ import com.movie.test.service.LonginService;
 @Service
 public class LonginServiceProc implements LonginService {
 
-	@Autowired
-	MemberRepository repository;
+	@Autowired //fixme 클래스에 롬복 어노테이션 사용해서 @Autowired는 생략 가능하니 참고해주세요.
+	MemberRepository repository; //fixme 특별한 이유가 없다면 접근제어는 가능한 엄격하게 해주세요. private
 	
 	@Override
 	public void save(SignupDataDTO dto, Model model) {
