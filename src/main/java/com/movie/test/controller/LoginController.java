59efcb9523ac7e4ad24627e138612fd0356dfcee
TaeJoon Jeon <46275680+DingDongDeng@users.noreply.github.com>
2022-06-25 23:23:43 +0900

    Update LoginController.java

--- src/main/java/com/movie/test/controller/LoginController.java
+++ src/main/java/com/movie/test/controller/LoginController.java
@@ -28,9 +28,14 @@ public class LoginController {
 	}
 	
 	
+	
+	//fixme 같은 DTO모델을 signin과 signup에서 공유하고 있네요. 내용이 같아보여도 실제론 다른 용도의 모델이니 구분되어야합니다.
+	// 만약 회원가입할때 파라미터 변경이 필요하다고 상상해보세요.
+	// 회원가입 로직을 수정했는데, 로그인 로직까지 영향을 받는 안좋은 구조가 될거같아요.
+	
 	//비즈니스 로직
 	@PostMapping("/login/signup")
-	public String signup(SignupDataDTO dto, Model model) {
+	public String signup(SignupDataDTO dto, Model model) { 
 		System.out.println(dto);
 		service.save(dto, model);
 		return "redirect:/";
