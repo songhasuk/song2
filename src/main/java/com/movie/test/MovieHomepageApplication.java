2f9dcb36f461e1a6a78219dc9106746e39755645
TaeJoon Jeon <46275680+DingDongDeng@users.noreply.github.com>
2022-06-25 23:06:07 +0900

    Update MovieHomepageApplication.java

--- src/main/java/com/movie/test/MovieHomepageApplication.java
+++ src/main/java/com/movie/test/MovieHomepageApplication.java
@@ -3,7 +3,7 @@ package com.movie.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-@EnableJpaAuditing 
+@EnableJpaAuditing  //fixme JPA 관련 어노테이션이니 따로 설정 클래스로 관리하는게 좋을거같아요
 @SpringBootApplication
 public class MovieHomepageApplication {
 
