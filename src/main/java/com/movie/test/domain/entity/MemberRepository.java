43cf7442be9680acb65b9286b025aae31d991abe
TaeJoon Jeon <46275680+DingDongDeng@users.noreply.github.com>
2022-06-25 23:18:09 +0900

    Update MemberRepository.java

--- src/main/java/com/movie/test/domain/entity/MemberRepository.java
+++ src/main/java/com/movie/test/domain/entity/MemberRepository.java
@@ -1,4 +1,4 @@
-package com.movie.test.domain.entity;
+package com.movie.test.domain.entity; //fixme 레포지토리 클래스가 entity 패키지에 있는데 entity와 repository 개념은 분리되는게 더 좋을거같아요
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
